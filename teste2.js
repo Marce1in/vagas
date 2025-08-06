9039de1a0f0a89057385f9a4da253193bf83cc09
Marce1in <marcelo.desenvolvedor.contato@gmail.com>
2025-08-06 16:02:29 -0300

    refactor: solve 'teste2' challenge

--- teste2.js
+++ teste2.js
@@ -1,17 +1,23 @@
-import data from './fakeData.js'
+import data from './fakeData.js';
+import { findLastUserId } from './helpers.js';
 
-function teste2 (req, res) {
-    var name = req.body.name;
-    var jov = req.body.job;
+function teste2(req, res) {
+    const id = findLastUserId(data) + 1
+    const name = req.body.name;
+    const job = req.body.job;
 
-    var newUser = {
+    if (!name || !job){
+        res.status(400).send({ error: 'name and job parameters are required' });
+    }
+
+    const newUser = {
+        id: id,
         name: name,
         job: job,
     };
 
     data.push(newUser);
-
     res.send(newUser);
-};
+}
 
-export default teste2
+export default teste2;
