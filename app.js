5e4e97269bed7d4a11df740efdc4da25c7f44f0d
Marce1in <marcelo.desenvolvedor.contato@gmail.com>
2025-08-06 13:46:13 -0300

    refactor: remove bodyParser because now Express already comes with

--- app.js
+++ app.js
@@ -10,10 +10,9 @@ const app = express()
 app.set('view engine', 'jade');
 
 app.use(express.json());
-app.use(express.urlencoded());
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(__dirname + '/public'));
 
