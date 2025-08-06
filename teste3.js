113fe9901f72fa775378af508fb66c93cdf3a791
Marce1in <marcelo.desenvolvedor.contato@gmail.com>
2025-08-06 16:02:41 -0300

    refactor: solve 'teste3' challenge

--- teste3.js
+++ teste3.js
@@ -1,15 +1,22 @@
-import data from './fakeData.js'
+import data from './fakeData.js';
 
-function teste3 (req, res) {
-    var name = req.query.name;
-
-    for (let i = 0; i < data.length; i++) {
-        if (i.name == name) {
-            data[i] = null;
-        }
+function teste3(req, res) {
+    const name = req.query.name;
+    if (!name) {
+        res.status(400).send({ error: 'name parameter is required' });
     }
 
-    res.send('success');
-};
+    const userIndex = data.findIndex((user) => user.name === name);
+    const deletedUser = data.splice(userIndex, 1)[0];
 
-export default teste3
+    if (!deletedUser) {
+        res.status(404).send({ error: 'User not found' });
+    }
+
+    res.send({
+        message: 'success',
+        user: deletedUser,
+    });
+}
+
+export default teste3;
