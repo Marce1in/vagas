91fac7c091f77893f36b29590ff2b750942c5877
Marce1in <marcelo.desenvolvedor.contato@gmail.com>
2025-08-06 16:03:44 -0300

    refactor: solve 'teste4' challenge

--- teste4.js
+++ teste4.js
@@ -1,13 +1,25 @@
-import data from "./fakeData.js"
+import data from './fakeData.js';
 
-function teste4 (req, res) {
-    var id = req.query.id;
+function teste4(req, res) {
+    const id = req.query.id;
+    const name = req.body.name;
+    const job = req.body.job;
 
-    const reg = data.find((d) => id == id);
-    reg.name = req.body.name;
-    reg.job = req.body.job;
+    if (!id || !name || !job) {
+        res.status(400).send({
+            error: 'id, name and job parameters are required',
+        });
+    }
 
-    res.send(reg);
-};
+    const user = data.find((user) => user.id == id);
 
-export default teste4
+    user = {
+        id: id,
+        name: name,
+        job: job,
+    };
+
+    res.send(user);
+}
+
+export default teste4;
