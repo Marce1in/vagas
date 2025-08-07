c1ad84b34332cc0b0b5d4e43e730d7688462c519
Marce1in <marcelo.desenvolvedor.contato@gmail.com>
2025-08-06 23:03:18 -0300

    feat: add 'teste6' route and middleware to app

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import teste2 from './teste2.js';
 import teste3 from './teste3.js';
 import teste4 from './teste4.js';
 import teste5 from './teste5.js';
+import * as teste6 from './teste6.js'
 
 const app = express();
 
@@ -18,15 +19,17 @@ app.get('/', function(req, res) {
   post users/ </br>
   delete users/ </br>
   put users/ </br>
+  post login/ </br>
   `);
 });
 
 app.get('/user', teste1.getUser);
 app.get('/users', teste1.getUsers);
 app.post('/users', teste2);
-app.delete('/users', teste3);
-app.put('/users', teste4);
+app.delete('/users', teste6.authMiddleware, teste3);
+app.put('/users', teste6.authMiddleware, teste4);
 app.get('/users/access', teste5);
+app.post('/login', teste6.login);
 
 const port = 3000;
 app.listen(port, function() {
