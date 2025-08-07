a2f64e70fdcc99481fb259f5a001d32b6c0939ac
Marce1in <marcelo.desenvolvedor.contato@gmail.com>
2025-08-06 22:51:51 -0300

    feat: solve 'teste6' challenge

--- teste6.js
+++ teste6.js
@@ -1,4 +1,5 @@
 import fakeCache from './fakeCache';
+import randomUUID from crypto
 
 export function authMiddleware(req, res, next) {
     const token = req.headers.authorization;
@@ -9,7 +10,23 @@ export function authMiddleware(req, res, next) {
         res.status(401).send({ error: 'invalid token' });
     }
 
-    req.permissions = fakeCache.tokens[token]
+    req.permissions = fakeCache.tokens[token];
 
-    next()
+    next();
+}
+
+export function login(req, res) {
+    const editPermission = Boolean(req.body.canEdit);
+    const deletePermission = Boolean(req.body.canEdit);
+
+    const permissions = {
+        canEdit: editPermission,
+        canDelete: deletePermission,
+    };
+
+    const token = randomUUID()
+
+    fakeCache.tokens[token] = permissions
+
+    res.send({ authToken: token })
 }
